29b0b25b28a023aaac1aa1249d14b9045e087530
evanrich2404 <evanrich2404@gmail.com>
2023-04-13 19:30:09 -0500

    mixed them up because bad name, got it right now

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,13 +27,13 @@ size_t binary_tree_height_2(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0, cmp = 0;
+	size_t left_Total = 0, right_Total = 0, cmp = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	right_height = binary_tree_height_2(tree->right);
-	left_height = binary_tree_height_2(tree->left);
-	cmp = right_height - left_height;
+	right_Total = binary_tree_height_2(tree->right);
+	left_Total = binary_tree_height_2(tree->left);
+	cmp = left_Total - right_Total;
 	return (cmp);
 }
